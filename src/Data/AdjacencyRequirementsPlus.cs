d02d1d2b16e09c6f4c0714215cf5a1a96453a86f
Maksym Muraviov$ <gpluiy@gmail.com>
2026-03-10 19:31:58 +0100

    Start from scratch

--- src/Data/AdjacencyRequirementsPlus.cs
+++
@@ -1,9 +0,0 @@
-using Polytopia.Data;
-
-namespace PolyPlus.Data
-{
-    public class AdjacencyRequirementsPlus
-    {
-        public TileData.EffectType effect { get; set; } = TileData.EffectType.None;
-    }
-}
--- src/Data/DiplomacyDataPlus.cs
+++
@@ -1,9 +0,0 @@
-namespace PolyPlus.Data
-{
-    public class DiplomacyDataPlus
-    {
-        public int peaceMultiplier { get; set; } = 1;
-        public int embassyCostModifierPerEmbassy { get; set; } = 0;
-        public int maxRebelCount { get; set; } = 5;
-    }
-}
